6991d1100618dde81e8921bea0aba070258697dd
Cedrick <cedrickvalencia811@gmail.com>
2022-06-13 16:45:20 +0800

    Create function that takes an array of ages

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,17 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+# [25, 10, 8, 20, 19]
+def calculate_ageDiff(ages)
+  age_diff_array = []
+
+  ages.each_with_index do |age, index|
+    unless ages[index + 1].nil?
+      age_diff = age - ages[index + 1]
+      age_diff_array << age_diff
+    end
+  end
+  age_diff_array
+end
+
+p calculate_ageDiff([25, 10, 8, 20, 19])
