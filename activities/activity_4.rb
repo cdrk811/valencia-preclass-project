0cb322fe95a139127162b9dc5ada2b92466b1c3a
Cedrick <cedrickvalencia811@gmail.com>
2022-06-13 16:49:51 +0800

    Create function for single ages

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,19 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+def single_number(ages)
+  uniqueArray = []
+  ages.each do |age|
+    ages.each do |age2|
+      if age == age2
+        count = count + age2
+      end
+    end
+    if count == 1
+      uniqueArray << age
+    end
+    uniqueArray
+  end
+end
+
+p single_number([31, 21, 19, 24, 31, 19])
